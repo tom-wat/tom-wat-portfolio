bc21c0ac755e3eea32c596d3d1964d989ff299ca
tom-wat <52376398+tom-wat@users.noreply.github.com>
2024-09-08 23:37:44 +0900

    文字表示の調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://tom-wat.github.io/",
   base: "/portfolio",
+  integrations: [icon()],
 });
