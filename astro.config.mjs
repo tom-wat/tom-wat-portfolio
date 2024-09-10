27362940f5777d507375485432239bcb547970c7
tom-wat <52376398+tom-wat@users.noreply.github.com>
2024-09-10 17:45:19 +0900

    設定ファイルの変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 export default defineConfig({
-  site: "https://tom-wat.github.io/",
-  base: "/portfolio",
+  site: "https://tom-wat-portfolio.vercel.app/",
+  base: "/",
   integrations: [icon()],
 });
